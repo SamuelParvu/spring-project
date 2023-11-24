d2df76bdfd07f12c8f7d54f01ef0888b575e28a2
Samuel Parvu <samuel.parvu.work@gmail.com>
2023-11-25 00:33:30 +0200

    still forgot to switch

---
+++ src/main/java/ro/itschool/project/services/UserServiceImpl.java
@@ -0,0 +1,45 @@
+package ro.itschool.project.services;
+
+import org.springframework.stereotype.Service;
+import ro.itschool.project.models.dtos.UserDTO;
+import ro.itschool.project.models.entities.User;
+import ro.itschool.project.repositories.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDTO createUser(UserDTO userDTO) {
+        // validate DTO
+        // convert DTO to entity
+        // save entity to database
+
+        // validation
+        if (userDTO.getFirstName().length() < 3){
+            throw new IllegalArgumentException("Invalid first name.");
+        }
+
+        // conversion to entity
+        User userEntity = new User();
+        userEntity.setFirstName(userDTO.getFirstName());
+        userEntity.setLastName(userDTO.getLastName());
+        userEntity.setEmail(userDTO.getEmail());
+
+        User savedUser = userRepository.save(userEntity);
+
+        // conversion to DTO
+        UserDTO userResponseDTO = new UserDTO();
+        userResponseDTO.setId(savedUser.getId());
+        userResponseDTO.setFirstName(savedUser.getFirstName());
+        userResponseDTO.setLastName(savedUser.getLastName());
+        userResponseDTO.setEmail(savedUser.getEmail());
+
+        return userResponseDTO;
+    }
+}
