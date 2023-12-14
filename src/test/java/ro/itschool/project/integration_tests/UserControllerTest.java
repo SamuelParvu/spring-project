262cc721f536e008eb145316fe2f98ba2278c468
Samuel Parvu <samuel.parvu.work@gmail.com>
2023-12-14 03:48:58 +0200

    integration test, first commit

---
+++ src/test/java/ro/itschool/project/integration_tests/UserControllerTest.java
@@ -0,0 +1,30 @@
+package ro.itschool.project.integration_tests;
+
+import jakarta.transaction.Transactional;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@Slf4j
+@Transactional
+@AutoConfigureTestDatabase
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testCreateUserShouldPass(){
+
+    }
+}
